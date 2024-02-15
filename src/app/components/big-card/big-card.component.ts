65eb29f0b23537175b1819239336ecd4dd161190
adaoma11 <adaoma11@hotmail.com>
2024-02-15 14:43:38 -0300

    created dynamic behavior to the components

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
 
-  imageUrl:string = 'https://img.freepik.com/fotos-gratis/microchip-em-extreme-closeup_125540-4902.jpg?w=900&t=st=1707960668~exp=1707961268~hmac=1230b4acc5036e2fbc1924024a69a0270e65ae81d0b6dbf1e506407d04417705';
-  creditsText:string = '';
-  title:string = 'Lorem ipsum dolor sit amet';
-  text:string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quia provident iste molestias! Adipisci sapiente sunt laborum recusandae.';
+  @Input() imageUrl:string = '';
+  @Input() creditsUrl:string = '';
+  @Input() title:string = '';
+  @Input() text:string = '';
 
   constructor() { }
 
